cac74e1b518425c6e3218b5bf5af5380b030e945
garciadd <garciad@ifca.unican.es>
2017-12-14 16:38:31 +0100

    Update band_S2.py

--- band_S2.py
+++ band_S2.py
@@ -8,7 +8,7 @@ Created on Mon Dec 11 13:12:06 2017
 import numpy as np
 
 import sys
-sys.path.append('/home/daniel/.snap/snap-python')
+sys.path.append('/home/user/.snap/snap-python')
 from snappy import (ProductIO, jpy, GPF, HashMap)
 
 path='...'
@@ -21,10 +21,10 @@ print("Bands:   %s" % (list(p.getBandNames())))
 
 def band_to_array(band_name, path):
 
-#leemos el producto
+    #leemos el producto
     p = ProductIO.readProduct(path)
 
-#cambiamos la relsolucion de las bandas. Resample
+    #cambiamos la relsolucion de las bandas. Resample
     GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
     HashMap = jpy.get_type('java.util.HashMap')
     BandDescriptor = jpy.get_type('org.esa.snap.core.gpf.common.BandMathsOp$BandDescriptor')
@@ -39,7 +39,7 @@ def band_to_array(band_name, path):
     
     print "Resample ok!"
     
-#Escogemos el poligono que necesitamos en vez de tratar toda la imagen
+    #Escogemos el poligono que necesitamos en vez de tratar toda la imagen
     GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
     WKTReader = jpy.get_type('com.vividsolutions.jts.io.WKTReader')
     wkt="POLYGON((-2.83292 41.82416,-2.69014 41.82416,-2.69014 41.90993,-2.83292 41.90993,-2.83292 41.82416))"
@@ -53,11 +53,11 @@ def band_to_array(band_name, path):
     
     print "subset ok!"
     
-#leemos la banda
+    #leemos la banda
     band=subset.getBand(band_name)
     print band
 
-#creamos una imagen de la banda formato png.
+    #creamos una imagen de la banda formato png.
     ImageManager=jpy.get_type('org.esa.snap.core.image.ImageManager')
     JAI=jpy.get_type('javax.media.jai.JAI')
     
@@ -65,7 +65,7 @@ def band_to_array(band_name, path):
     JAI.create("filestore", im,'%s.png' % (band_name), 'PNG')
     print "PNG Done!"
 
-#creamos una imagen de la banda formato GeoTIFF
+    #creamos una imagen de la banda formato GeoTIFF
     parameters = HashMap()
     parameters.put('sourceBands', band_name)
     result = GPF.createProduct('Subset', parameters, subset)
